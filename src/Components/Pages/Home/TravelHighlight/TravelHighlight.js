c2018a879beba3154e60ea168458e7a6e1a81e0c
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2022-02-08 13:28:38 +0600

    Dl

---
+++ src/Components/Pages/Home/TravelHighlight/TravelHighlight.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { HiOutlinePlay } from "react-icons/hi";
+
+const TravelHighlight = () => {
+    return (
+        <div>
+            <div>
+                <h1>Traveling Highlights</h1>
+                <p>Your New Traveling Idea</p>
+                <HiOutlinePlay />
+            </div>
+        </div>
+    );
+};
+
+export default TravelHighlight;
